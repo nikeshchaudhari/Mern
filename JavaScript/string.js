06de4e8125661ad7afb92495851117e87e061bfe
Nikesh Chaudhari <info.nikesh12@gmail.com>
2024-08-21 15:37:10 +0545

    Learn

---
+++ JavaScript/string.js
@@ -0,0 +1,11 @@
+// let text = "   Welcome to  js\\nJavaScript  ";
+
+// // String Method
+// console.log(text.trimStart());
+
+
+const num = 14256;
+let str = num.toString();
+console.log(str,typeof str);
+
+
