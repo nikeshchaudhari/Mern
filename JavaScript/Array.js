b4bccdb64d7c9a5b6e0592d9035fd8dc353db84b
Nikesh Chaudhari <info.nikesh12@gmail.com>
2024-08-21 15:32:51 +0545

    js

---
+++ JavaScript/Array.js
@@ -0,0 +1,42 @@
+// Collection Of Data
+
+
+// const students = ["Roshan","Aman","Chiran","rahul"]
+// console.log(students[0]);
+
+
+// const students=[
+//     {
+//         name : "Nikesh",
+//         roll : 11,
+//         marks : 65
+
+//     },
+//     {
+//         name : "Rahul",
+//         roll : 12,
+//         marks : 56
+//     }
+// ]
+
+// console.log(students[0].name);
+
+// Array Length 
+const person = ["Nikesh","Bikash","Gopal","Hari"];
+person.reverse();
+console.log(person);
+
+// person.push("Avi");
+
+
+// console.log(person.short);
+// const newperson = person.slice(2);
+// console.log(newperson);
+
+const number  =[20,4,0,10,5,89];
+number[0]= 59;
+console.log(number);
+
+
+
+
