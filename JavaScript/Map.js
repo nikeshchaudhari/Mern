c527b2887caa2d95a0f0c0bb836dce208d9f1d88
Nikesh Chaudhari <info.nikesh12@gmail.com>
2024-08-21 15:36:39 +0545

    Learn

---
+++ JavaScript/Map.js
@@ -0,0 +1,20 @@
+// return New Array In
+
+const number = [3,5,4,7];
+
+const num1 = number.map(function(value){
+return value*2;
+
+});
+
+console.log(num1);
+
+
+const person = ["rajesh","Rohan"
+];
+
+const per1 = person.map(function(value){
+return "Mr."+ value;
+})
+
+console.log(per1[0]);
