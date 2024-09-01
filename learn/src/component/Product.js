6c310c5180b73ede73f439b5a81fde28355bd43d
Nikesh Chaudhar <info.nikesh12@gmail.com>
2024-09-01 20:39:49 +0545

    React

--- learn/src/component/Product.js
+++ learn/src/component/Product.js
@@ -1,25 +1,16 @@
+const Product=()=>{
 
-const Product =(props)=>{
-   
-    
+    const demo =()=>{
+        console.log("Buy click");
+        
+    }
     return(
-        <div>
-           <h1>{props.title}</h1>
-           <p>Price :- {props.price}</p>
-           {props.children}
-           <Demo/>
-           
-        </div>
+        <>
+            <h1 className="prod">This is Product Component</h1>
+            <button onClick={demo}>Buy Now</button>
+        </>
     )
 }
 
-const Demo =()=>{
-return(
 
-    <div>
-        <h1>This is Profile Box</h1>
-        <p>This is demo box</p>
-    </div>
-)
-}
 export default Product;
